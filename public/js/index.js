c58d3ec193dd5460881f8431314c0cac33d9c992
mae.sunji@gmail.com <mae.sunji@gmail.com>
2017-07-08 02:26:38 +0800

    Sorting things out

--- public/js/index.js
+++ public/js/index.js
@@ -6,3 +6,12 @@ console.log('Application started')
 if (module.hot) {
   module.hot.accept();
 }
+
+// Accordion
+$(function(){
+  $('.panel-toggle').on('click', function() {
+    if($('.panel-toggle').hasClass("inactive")) {
+      alert('inactive');
+    }
+  });
+});
