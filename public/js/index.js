688fdce6ed08ab9f8e193dbf9328809488936558
mae.sunji@gmail.com <mae.sunji@gmail.com>
2017-07-08 10:47:49 +0800

    Sorted out first plans accordion

--- public/js/index.js
+++ public/js/index.js
@@ -7,11 +7,19 @@ if (module.hot) {
   module.hot.accept();
 }
 
-// Accordion
-$(function(){
-  $('.panel-toggle').on('click', function() {
-    if($('.panel-toggle').hasClass("inactive")) {
-      alert('inactive');
-    }
-  });
+$('.panel-toggle').on('click', function(){
+  var panelContent = $(this).next('.panel-content');
+  var nextContent = $(this).nextAll('.panel-toggle:first').next();
+
+  if($(this).hasClass('inactive')) {
+    panelContent.removeClass('closed').addClass('open');
+    $(this).removeClass('inactive').addClass('active');
+    $('.open').not(panelContent).addClass('closed').removeClass('open');
+    $('.active').not($(this)).addClass('inactive').removeClass('active');
+  } else {
+    panelContent.removeClass('open').addClass('closed');
+    $(this).addClass('inactive').removeClass('active');
+    $(this).nextAll('.panel-toggle:first').addClass('active').removeClass('inactive');
+    nextContent.removeClass('closed').addClass('open');
+  }
 });
