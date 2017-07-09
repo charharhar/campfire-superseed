4be258f71be8c704aad1cec4eaf499204ce4f481
mae.sunji@gmail.com <mae.sunji@gmail.com>
2017-07-09 08:53:17 +0800

    Changed coordinates, more zoomed in view

--- public/js/index.js
+++ public/js/index.js
@@ -7,8 +7,27 @@ if (module.hot) {
   module.hot.accept();
 }
 
-/* Accordion */
+/* Plans accordion */
+$('.panel-toggle').on('click', function(){
+  var panelContent = $(this).next('.panel-content');
+  var nextContent = $(this).nextAll('.panel-toggle:first').next();
+  var chevronArrow = $(this).find('i');
 
+  if($(this).hasClass('inactive')) {
+    panelContent.removeClass('closed').addClass('open');
+    $(this).removeClass('inactive').addClass('active');
+    $('.open').not(panelContent).addClass('closed').removeClass('open');
+    $('.active').not($(this)).addClass('inactive').removeClass('active');
+    chevronArrow.addClass('fa-chevron-up').removeClass('fa-chevron-down');
+  } else {
+    panelContent.removeClass('open').addClass('closed');
+    $(this).addClass('inactive').removeClass('active');
+    $(this).nextAll('.panel-toggle:first').addClass('active').removeClass('inactive');
+    nextContent.removeClass('closed').addClass('open');
+  }
+});
+
+/* Location accordion*/
 $('.panel-toggle').on('click', function(){
   var panelContent = $(this).next('.panel-content');
   var nextContent = $(this).nextAll('.panel-toggle:first').next();
@@ -29,11 +48,13 @@ $('.panel-toggle').on('click', function(){
 });
 
 /* Google Maps */
+/* HK */
 window.onload = function showHK() {
-  var hk = new google.maps.LatLng(22.2759, 114.1455);
+  var hk = new google.maps.LatLng(22.248337, 114.166833);
   var hkMap = new google.maps.Map(document.querySelector(".hk-map"),{
     center: hk,
-    zoom: 14
+    zoom: 15,
+    mapTypeControl: false
   });
   var marker = new google.maps.Marker({
     position: hk,
@@ -41,4 +62,10 @@ window.onload = function showHK() {
   });
 };
 
-// showHK();
+/* Australia */
+
+/* Singapore */
+
+/* UK */
+
+
