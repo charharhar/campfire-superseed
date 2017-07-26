7cabd8c2eb7e5b55c015aca3624a50e77e15c0e8
mae.sunji@gmail.com <mae.sunji@gmail.com>
2017-07-26 23:12:24 +0800

    Fixed location tabs and some other things

--- public/js/index.js
+++ public/js/index.js
@@ -42,121 +42,12 @@ $('.plans-toggle').on('click', function(){
   }
 });
 
-/* Location Tab Swap */
-var HK_Locations = {
-  HK_1: {
-    open: true,
-    name: 'Kennedy Town',
-    tableData: {
-      industry: 'tech',
-      address: '4/F, Cheung Hing Industrial Building, <br> 12P Smithfield, Kennedy Town <br> Hong Kong',
-      contact: 'hello.kt@campfire.work',
-    },
-    images: [
-      '../images/section-locations/photo1.png',
-      '../images/section-locations/photo2.png',
-      '../images/section-locations/photo3.png',
-    ],
-  },
-  HK_2: {
-    open: true,
-    name: 'Wong Chuk Hang',
-    tableData: {
-      industry: 'fashion',
-      address: '5/F, Remex Centre, <br> 42 Wong Chuk Hang Road, <br> Hong Kong',
-      contact: 'hello.wch@campfire.work'
-    },
-    images: [
-      '../images/section-locations/photo1.png',
-      '../images/section-locations/photo2.png',
-      '../images/section-locations/photo3.png',
-    ],
-  },
-  HK_3: {
-    open: false,
-    name: 'Quarry Bay',
-    tableData: {
-      message: 'Coming Soon',
-    },
-    images: [
-      '../images/section-locations/photo1.png',
-      '../images/section-locations/photo2.png',
-      '../images/section-locations/photo3.png',
-    ],
-  },
-  HK_4: {
-    open: false,
-    name: 'Tai Koo',
-    tableData: {
-      message: 'Coming Soon',
-    },
-    images: [
-      '../images/section-locations/photo1.png',
-      '../images/section-locations/photo2.png',
-      '../images/section-locations/photo3.png',
-    ],
-  },
-  HK_5: {
-    open: false,
-    name: 'Sham Shui Po',
-    tableData: {
-      message: 'Coming Soon',
-    },
-    images: [
-      '../images/section-locations/photo1.png',
-      '../images/section-locations/photo2.png',
-      '../images/section-locations/photo3.png',
-    ],
-  },
-  HK_6: {
-    open: false,
-    name: 'Tseung Kwan O',
-    tableData: 'Coming Soon',
-  },
-    images: [
-      '../images/section-locations/photo1.png',
-      '../images/section-locations/photo2.png',
-      '../images/section-locations/photo3.png',
-    ],
-  }
-
-
-var locationTab = $('.location-tab');
-var tabList = $('.tab-list');
-var locationWrapper = $('.location-column');
-
-// Generate modal windows
-function generateAddressTable(data) {
-  return `
-    <table class="white-text address-table small-text">
-      <tr>
-          <td class="table-left">Industry:</td>
-          <td>${data.industry}</td>
-      </tr>
-      <tr>
-          <td class="table-left">Address:</td>
-          <td>${data.address}</td>
-      </tr>
-      <tr>
-          <td class="table-left">Contact:</td>
-          <td>${data.contact}</td>
-      </tr>
-  </table>
-  `
-}
-
-function generateComingSoon() {
-  return `
-    <h1 class="white-text large-text">Coming Soon</h2>
-    <h2 class="white-text normal-text">Be the first to know!</h2>
-  `
-}
 /* Google Maps */
 /* HK */
-var HK_Map_Details = [{
-  mapTarget: '#hk-map',
+const HK_Map_Details = {
+  map_id: '#hk-map',
   center: {lat: 22.248337, lng: 114.166833},
-  zoom: 15,
+  zoom: 12,
   markers: [
     // Kennedy Town
     {
@@ -172,36 +63,36 @@ var HK_Map_Details = [{
     },
     // Quarry Bay
     {
-      coords: {lat: 22.248337, lng: 114.166833},
+      coords: {lat: 22.287823, lng: 114.209737},
       content: 'HK_3',
       target: '#hk_content'
     },
     // Tai Koo
     {
-      coords: {lat: 22.248337, lng: 114.166833},
+      coords: {lat: 22.284733, lng: 114.215546},
       content: 'HK_4',
       target: '#hk_content'
     },
     // Sam Shui Po
     {
-      coords: {lat: 22.248337, lng: 114.166833},
+      coords: {lat: 22.331028, lng: 114.161816},
       content: 'HK_5',
       target: '#hk_content'
     },
     // Tseung Kwan O
     {
-      coords: {lat: 22.248337, lng: 114.166833},
+      coords: {lat: 22.307441, lng: 114.260011},
       content: 'HK_6',
       target: '#hk_content'
     },
   ],
-}]
+}
 
 /* AU */
-var AU_Map_Details = [{
-  mapTarget: '#au-map',
+const AU_Map_Details = {
+  map_id: '#au-map',
   center: {lat: -33.8688, lng: 151.2093},
-  zoom: 12,
+  zoom: 9,
   markers: [
     {
       coords: {lat: -33.8688, lng: 151.2093},
@@ -209,25 +100,25 @@ var AU_Map_Details = [{
       target: '#au_content'
     },
   ],
-}]
+}
 
 /* SG */
-var SG_Map_Details = [{
-  mapTarget: '#sg-map',
-  center: {lat: 1.3554, lng: 103.8677},
-  zoom: 12,
-  markers: [
-    {
-      coords: {lat: 1.3554, lng: 103.8677},
-      content: 'SG_1',
-      target: '#sg_content'
-    },
-  ],
-}]
+// const SG_Map_Details = {
+//   map_id: '#sg-map',
+//   center: {lat: 1.3554, lng: 103.8677},
+//   zoom: 12,
+//   markers: [
+//     {
+//       coords: {lat: 1.3554, lng: 103.8677},
+//       content: 'SG_1',
+//       target: '#sg_content'
+//     },
+//   ],
+// }
 
 /* UK*/
-var UK_Map_Details = [{
-  mapTarget: '#uk-map',
+const UK_Map_Details = {
+  map_id: '#uk-map',
   center: {lat: 51.5074, lng: -0.1278},
   zoom: 12,
   markers: [
@@ -237,10 +128,10 @@ var UK_Map_Details = [{
       target: '#uk_content'
     },
   ],
-}]
+}
 
 // Map options -- zoom, streetview control, etc. go here
-function mapOptions(center, zoom) {
+function setMapOptions(center, zoom) {
   return {
     center: center,
     zoom: zoom,
@@ -251,23 +142,22 @@ function mapOptions(center, zoom) {
 }
 
 // Add location markers to map
-
 function addMarker(point, map) {
-  var point = {coords, content, target};
-  var icon = {
+  const {coords, content, target} = point;
+  const icon = {
     url: '../images/section-locations/pin.png',
-    scaledSize: new google.maps.Size(40,60),
+    scaledSize: new google.maps.Size(33,50),
   }
 
-  var marker = new google.maps.Marker({
+  const marker = new google.maps.Marker({
     position: coords,
     map: map,
-    icon,
+    icon: icon,
   })
 
   // Add event listener to map markers
   google.maps.event.addListener(marker, 'click', (e) => {
-    var targetTab = document.querySelector(`[data-context="${content}"]`);
+    const targetTab = document.querySelector(`[data-context="${content}"]`);
     !!targetTab && locationTabChange(targetTab, target);
   });
 }
@@ -275,11 +165,10 @@ function addMarker(point, map) {
 // Initialise map
 window.addEventListener('load', function(){
 
-  function showMap(options) {
-    var mapTarget =
-    var options = {mapTarget, center, zoom, markers};
-    var mapOptions = mapOptions(center, zoom);
-    var map = new google.maps.Map(document.querySelector(mapTarget), mapOptions);
+  function startMap(options) {
+    const {map_id, center, zoom, markers} = options;
+    const mapOptions = setMapOptions(center, zoom);
+    const map = new google.maps.Map(document.querySelector(map_id), mapOptions);
 
     markers.forEach(marker => {
       addMarker(marker, map)
@@ -287,20 +176,203 @@ window.addEventListener('load', function(){
   }
 
   // Initialise maps
-  showMap(HK_Map_Details);
-  showMap(AU_Map_Details);
-  // showMap(SG_Map_Details);
-  showMap(UK_Map_Details);
+  startMap(HK_Map_Details);
+  startMap(AU_Map_Details);
+  // startMap(SG_Map_Details);
+  startMap(UK_Map_Details);
+
 });
 
+/* Location Tab Swap */
+var HK_Locations = {
+  HK_1: {
+    open: true,
+    name: 'Kennedy Town',
+    tableData: {
+      industry: 'Tech',
+      address: '4/F, Cheung Hing Industrial Building, <br> 12P Smithfield, Kennedy Town <br> Hong Kong',
+      contact: 'hello.kt@campfire.work',
+    },
+    images: [
+      '../images/section-locations/locations-kt/kt-1.png',
+      '../images/section-locations/locations-kt/kt-2.png',
+      '../images/section-locations/locations-kt/kt-3.png',
+      '../images/section-locations/locations-kt/kt-4.png',
+      '../images/section-locations/locations-kt/kt-5.png',
+    ],
+  },
+  HK_2: {
+    open: true,
+    name: 'Wong Chuk Hang',
+    tableData: {
+      industry: 'Fashion',
+      address: '5/F, Remex Centre, <br> 42 Wong Chuk Hang Road, <br> Hong Kong',
+      contact: 'hello.wch@campfire.work'
+    },
+    images: [
+      '../images/section-locations/locations-wch/wch-1.png',
+      '../images/section-locations/locations-wch/wch-2.png',
+      '../images/section-locations/locations-wch/wch-3.png',
+      '../images/section-locations/locations-wch/wch-4.png',
+      '../images/section-locations/locations-wch/wch-5.png'
+    ],
+  },
+  HK_3: {
+    open: false,
+    name: 'Quarry Bay',
+    tableData: {
+      message: 'Coming Soon',
+    },
+    images: [],
+  },
+  HK_4: {
+    open: false,
+    name: 'Tai Koo',
+    tableData: {
+      message: 'Coming Soon',
+    },
+    images: [],
+  },
+  HK_5: {
+    open: false,
+    name: 'Sham Shui Po',
+    tableData: {
+      message: 'Coming Soon',
+    },
+    images: [],
+  },
+  HK_6: {
+    open: false,
+    name: 'Tseung Kwan O',
+    tableData: 'Coming Soon',
+  },
+    images: [],
+  }
 
+const locationTabs = $('.location-tab');
+// const locationTabList = $('.tab-list');
 
+function generateAddressTable(data) {
+  // console.log(data);
+  return `
+    <div class="row tab-list-blank">
+    </div>
+    <div class="row">
+      <table class="white-text address-table small-text">
+        <tr>
+            <td class="table-left">Industry:</td>
+            <td>${data.industry}</td>
+        </tr>
+        <tr>
+            <td class="table-left">Address:</td>
+            <td>${data.address}</td>
+        </tr>
+        <tr>
+            <td class="table-left">Contact:</td>
+            <td>${data.contact}</td>
+        </tr>
+      </table>
+    </div>
+  `
+}
+
+function generateComingSoon() {
+  return `
+    <h1 class="white-text medium-text">COMING SOON</h1>
+    <h2 class="white-text normal-text">Be the first to know!</h2>
+    <a href="" class="button red-button normal-button">Contact Us</a>
+  `
+}
+
+function generateSlider(image, classType) {
+  const div = document.createElement('div');
+  const img = document.createElement('img');
+
+  img.setAttribute('src', image);
+  img.className = classType;
+  div.appendChild(img);
+
+  return div;
+}
+
+function locationTabChange(tab, targetID) {
+  const locationDetails = $('#hk-modal');
+  // const targetContent = document.querySelector(targetID);
+  const locationData = HK_Locations[tab.getAttribute('data-context')];
+  const locationLeft = document.createElement('div');
+  const locationRight = document.createElement('div');
+  const slider = document.createElement('div');
+  const sliderFor = document.createElement('div');
+  const sliderNav = document.createElement('div');
+
+  locationLeft.className = "col col-xs-4 location-left";
+  locationRight.className = "col col-xs-8 hidden-xs location-right";
+  slider.className = "slider";
+  sliderFor.className = "slider-for";
+  sliderNav.className = "slider-nav";
+
+  locationDetails.addClass("open-modal");
+
+  if(locationData.open) {
+    locationDetails.empty();
+    locationDetails.show();
+    $(locationLeft).appendTo(locationDetails);
+    $(locationRight).appendTo(locationDetails);
+    locationDetails.removeClass('coming-soon');
+
+    $(locationLeft).append(generateAddressTable(locationData.tableData));
+
+    locationRight.append(slider);
+
+    locationData.images.forEach(image => {
+      sliderFor.append(generateSlider(image, 'slider-img'))
+    })
+
+    locationData.images.forEach(image => {
+      sliderNav.append(generateSlider(image, 'slider-thumb'))
+    })
+
+    slider.append(sliderFor);
+    slider.append(sliderNav);
+
+    // Image slider
+     $('.slider-for').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      fade: true,
+      asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      asNavFor: '.slider-for',
+      centerMode: true,
+      arrows: true,
+      dots: false,
+      focusOnSelect: true
+    });
+  }
+  else {
+    locationDetails.show();
+    locationDetails.empty();
+    locationDetails.addClass("coming-soon");
+    locationDetails.append(generateComingSoon());
+  }
+}
+
+locationTabs.on('click', function() {
+  const tabDetails = $(this)[0];
+  console.log(tabDetails);
+  locationTabChange(tabDetails);
+});
 
 /* Location accordion*/
 $('.locations-toggle').on('click', function(){
   var locationContent = $(this).next('.locations-content');
   var nextLocation = $(this).nextAll('.locations-toggle:first').next();
   var locationChevron = $(this).find('.locations-chevron');
+
   if($(this).hasClass('inactive-location')) {
     locationContent.removeClass('closed-location').addClass('open-location');
     $(this).removeClass('inactive-location').addClass('active-location');
@@ -315,27 +387,13 @@ $('.locations-toggle').on('click', function(){
   }
 });
 
-/* Image slider */
- $('.slider-for').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  centerMode: true,
-  fade: true,
-  asNavFor: '.slider-nav'
-});
-$('.slider-nav').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  asNavFor: '.slider-for',
-  centerMode: true,
-  centerPadding: 10,
-  arrows: true,
-  dots: false,
-  focusOnSelect: true
+/* Close location details */
+$('.close-modal').on('click', function(){
+  console.log('okay');
+  $('#hk-modal').hide();
 });
 
-
+/* Stay in the loop background image thing */
 $('#loop-text-container').on('mousemove', function(e) {
   var leftmoveX = (e.pageX * - 1/15);
   var leftmoveY = (e.pageY * - 1/40);
